4902427877ee1c67fbf0b61ccf120c173f71fb14
Cebollino <33807515+Cebollino@users.noreply.github.com>
2019-05-15 23:39:08 +0100

    adding buttons and working with angular shortcuts, change material for bootstrap

--- mean-proyecto/src/app/app.module.ts
+++ mean-proyecto/src/app/app.module.ts
@@ -1,14 +1,22 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
+import { MatInputModule, MatCardModule } from  '@angular/material'
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
